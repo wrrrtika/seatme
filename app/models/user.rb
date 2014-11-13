66d316223af9da901f8c1d4ef6ac76ba5b13ea0e
Vincent Jim <vjim323@gmail.com>
2014-11-13 16:54:45 -0500

    created new user path and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 	has_one :restaurant
 	has_many :reservations
+	has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :user, only: [:new, :create]
+  resources :users, only: [:new, :create]
 
   resources :restaurants do
     resources :reservations
